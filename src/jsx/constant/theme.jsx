225504d835e6d98927e5896a9f71d43735a819ae
khushalhiranii <140611208+khushalhiranii@users.noreply.github.com>
2024-10-14 12:36:10 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/jsx/constant/theme.jsx
@@ -0,0 +1,129 @@
+import BigImg1 from '../../assets/images/big/img1.jpg'
+import Profile from '../../assets/images/profile/profile.png'
+
+import BigImg from '../../assets/images/big/img1.jpg'
+import BigImg5 from '../../assets/images/big/img5.jpg'
+import Avatar4 from '../../assets/images/avatar/4.jpg'
+import Avatar2 from '../../assets/images/avatar/2.jpg'
+import Avatar3 from '../../assets/images/avatar/3.jpg'
+import Avatar1 from '../../assets/images/avatar/1.jpg'
+import Avatpng5 from '../../assets/images/avatar/5.png'
+import Avatpng1 from '../../assets/images/avatar/1.png'
+
+import course1 from '../../assets/images/courses/pic1.jpg'
+import course2 from '../../assets/images/courses/pic2.jpg'
+import course3 from '../../assets/images/courses/pic3.jpg'
+import course4 from '../../assets/images/courses/pic4.jpg'
+import course5 from '../../assets/images/courses/pic5.jpg'
+import course6 from '../../assets/images/courses/pic6.jpg'
+import course7 from '../../assets/images/courses/pic7.jpg'
+import course8 from '../../assets/images/courses/pic8.jpg'
+
+import education1 from '../../assets/images/profile/education/pic1.jpg'
+import education2 from '../../assets/images/profile/education/pic2.jpg'
+import education3 from '../../assets/images/profile/education/pic3.jpg'
+import education4 from '../../assets/images/profile/education/pic4.jpg'
+import education5 from '../../assets/images/profile/education/pic5.jpg'
+import education6 from '../../assets/images/profile/education/pic6.jpg'
+import education7 from '../../assets/images/profile/education/pic7.jpg'
+import education8 from '../../assets/images/profile/education/pic8.jpg'
+import education9 from '../../assets/images/profile/education/pic9.jpg'
+import education10 from '../../assets/images/profile/education/pic10.jpg'
+
+import Profile8 from '../../assets/images/profile/8.jpg'
+import Profile9 from '../../assets/images/profile/9.jpg'
+import Profile10 from '../../assets/images/profile/10.jpg'
+
+import smallpic1 from '../../assets/images/profile/small/pic1.jpg'
+import smallpic2 from '../../assets/images/profile/small/pic2.jpg'
+import smallpic3 from '../../assets/images/profile/small/pic3.jpg'
+import smallpic4 from '../../assets/images/profile/small/pic4.jpg'
+import smallpic5 from '../../assets/images/profile/small/pic5.jpg'
+import smallpic6 from '../../assets/images/profile/small/pic6.jpg'
+import smallpic7 from '../../assets/images/profile/small/pic7.jpg'
+import smallpic8 from '../../assets/images/profile/small/pic8.jpg'
+import smallpic9 from '../../assets/images/profile/small/pic9.jpg'
+import smallpic10 from '../../assets/images/profile/small/pic10.jpg'
+
+import tab1 from '../../assets/images/tab/1.jpg'
+
+export const IMAGES = {
+    BigImg1: BigImg1,
+    Profile: Profile,
+
+    BigImg: BigImg,
+    BigImg5: BigImg5,
+    Avatar4: Avatar4,
+    Avatar2: Avatar2,
+    Avatar3: Avatar3,
+    Avatar1: Avatar1,
+    Avatpng5: Avatpng5,
+    Avatpng1: Avatpng1,
+
+    course1: course1,
+    course2: course2,
+    course3: course3,
+    course4: course4,
+    course5: course5,
+    course6: course6,
+    course7: course7,
+    course8: course8,
+
+    education1: education1,
+    education2: education2,
+    education3: education3,
+    education4: education4,
+    education5: education5,
+    education6: education6,
+    education7: education7,
+    education8: education8,
+    education9: education9,
+    education10: education10,
+
+    Profile8: Profile8,
+    Profile9: Profile9,
+    Profile10: Profile10,
+
+    smallpic1: smallpic1,
+    smallpic2: smallpic2,
+    smallpic3: smallpic3,
+    smallpic4: smallpic4,
+    smallpic5: smallpic5,
+    smallpic6: smallpic6,
+    smallpic7: smallpic7,
+    smallpic8: smallpic8,
+    smallpic9: smallpic9,
+    smallpic10: smallpic10,
+
+    tab1: tab1,
+
+};
+
+export const SVGICON = {
+    user: <svg id="icon-customers" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-user">
+        <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+        <circle cx="12" cy="7" r="4"></circle>
+    </svg>,
+    bills: <svg id="icon-orders" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-file-text">
+        <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+        <polyline points="14 2 14 8 20 8"></polyline>
+        <line x1="16" y1="13" x2="8" y2="13"></line>
+        <line x1="16" y1="17" x2="8" y2="17"></line>
+        <polyline points="10 9 9 9 8 9"></polyline>
+    </svg>,
+    revenue: <svg id="icon-revenue" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-dollar-sign">
+        <line x1="12" y1="1" x2="12" y2="23"></line>
+        <path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path>
+    </svg>,
+    grid: <svg id="icon-database-widget" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-database">
+        <ellipse cx="12" cy="5" rx="9" ry="3"></ellipse>
+        <path d="M21 12c0 1.66-4 3-9 3s-9-1.34-9-3"></path>
+        <path d="M3 5v14c0 1.66 4 3 9 3s9-1.34 9-3V5"></path>
+    </svg>,
+}
+
+const appTheme = {
+    IMAGES,
+    SVGICON,
+}
+export default appTheme;
